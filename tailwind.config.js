8dd4a76653e8e1089a5f72915a378048f965ebeb
ddyrnes <dev.dyrnes@gmail.com>
2025-01-23 10:22:58 +0100

    Added tailwind classes and fixed tailwind @apply

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
         primary: "#407CAF", // Brand primary color used for call-to-action elements
         disabled: "rgb(64, 124, 175 / 40%)", // Color for disabled or inactive buttons (same as primary but with opacity adjustments)
         background: "#E5EEF5", // Primary background color for a clean and modern interface
-        textWhite: "#FAFAFA", // White text for contrast against darker backgrounds
         inputGray: "#EFEFEF", // Light gray used for form inputs and neutral UI elements
-        textDark: "#403D3D", // Dark text color for readability on bright backgrounds
+        primaryText: "#FAFAFA", // Primary Text Color
+        dark: "#403D3D", // Dark text color for readability on bright backgrounds
       },
     },
   },
