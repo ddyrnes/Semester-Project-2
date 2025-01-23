7a04736846c58ae560017e9123e79491a167b11b
ddyrnes <dev.dyrnes@gmail.com>
2025-01-23 19:30:30 +0100

    added buttons for secondary nav on profile page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,10 @@ module.exports = {
       colors: {
         primary: "#407CAF", // Brand primary color used for call-to-action elements
         disabled: "rgb(64, 124, 175 / 40%)", // Color for disabled or inactive buttons (same as primary but with opacity adjustments)
-        primaryBackground: "#E5EEF5", // Primary background color for a clean and modern interface
+        primaryBackground: "#E5EEF5", // Primary background color
         input: "#EFEFEF", // Light gray used for form inputs and neutral UI elements
         primaryText: "#FAFAFA", // Primary Text Color
-        dark: "#403D3D", // Dark text color for readability on bright backgrounds
+        dark: "#403D3D", // Dark color for text and background
       },
       fontFamily: {
         sans: ["Roboto", "sans-serif"], // Set Roboto as the default font for everything
