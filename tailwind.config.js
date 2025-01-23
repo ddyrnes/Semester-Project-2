170405e12743e8aee1347dfca709a4c5d3e58b14
ddyrnes <dev.dyrnes@gmail.com>
2025-01-23 14:42:12 +0100

    Added example cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
       colors: {
         primary: "#407CAF", // Brand primary color used for call-to-action elements
         disabled: "rgb(64, 124, 175 / 40%)", // Color for disabled or inactive buttons (same as primary but with opacity adjustments)
-        background: "#E5EEF5", // Primary background color for a clean and modern interface
-        inputGray: "#EFEFEF", // Light gray used for form inputs and neutral UI elements
+        primaryBackground: "#E5EEF5", // Primary background color for a clean and modern interface
+        input: "#EFEFEF", // Light gray used for form inputs and neutral UI elements
         primaryText: "#FAFAFA", // Primary Text Color
         dark: "#403D3D", // Dark text color for readability on bright backgrounds
       },
