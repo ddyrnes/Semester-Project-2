ed4bb8e6317298eddd143e53b5a50b5126a54c62
ddyrnes <dev.dyrnes@gmail.com>
2025-01-23 09:43:21 +0100

    added custom tailwind classes for colors

--- README.md
+++ README.md
@@ -25,10 +25,11 @@ A front-end project for an **auction website**, built with **HTML, Tailwind CSS,
    ```sh
    npm install
    ```
-3. Build Tailwind CSS:
+3. Start Tailwind CSS:
    ```sh
-   npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch
+   npm start
    ```
+   # This will watch for file changes and automatically rebuild Tailwind when you save a file
 
 ## Author
 **Daniel Dyrnes**
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./pages/**/*.html", "./src/js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#407CAF", // Brand primary color used for call-to-action elements
+        disabled: "rgb(64, 124, 175 / 40%)", // Color for disabled or inactive buttons (same as primary but with opacity adjustments)
+        background: "#E5EEF5", // Primary background color for a clean and modern interface
+        textWhite: "#FAFAFA", // White text for contrast against darker backgrounds
+        inputGray: "#EFEFEF", // Light gray used for form inputs and neutral UI elements
+        textDark: "#403D3D", // Dark text color for readability on bright backgrounds
+      },
+    },
   },
   plugins: [],
 };
